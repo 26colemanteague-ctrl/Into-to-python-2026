918af1b4d60e34cee35e7636ee8ff1a5832a3894
26colemanteague-ctrl <26.coleman.teague@carolinaday.org>
2026-01-08 14:24:23 +0000

    Help!

--- programming-check-1.py
+++ programming-check-1.py
@@ -1,3 +1,8 @@
+input =("name")
 num=5
-if num>0:
+if num > 0:
     print ("you have a highr number than 0.")
+elif num<0:
+    print("you have a number less than 0.")
+else:
+    print("you number is equal to 0.")
