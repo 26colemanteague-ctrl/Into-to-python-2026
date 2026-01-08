ef5f50e4887f0f9a2006ea65d101a9998cefe584
26colemanteague-ctrl <26.coleman.teague@carolinaday.org>
2026-01-08 20:05:07 +0000

    Please work

--- programming-check-1.py
+++ programming-check-1.py
@@ -1,4 +1,3 @@
-input =("name")
 num=5
 if num > 0:
     print ("you have a highr number than 0.")
@@ -6,3 +5,8 @@ elif num<0:
     print("you have a number less than 0.")
 else:
     print("you number is equal to 0.")
+print
+print
+print("if you change the number change the num to what you want. than hit the play.")
+print
+print
