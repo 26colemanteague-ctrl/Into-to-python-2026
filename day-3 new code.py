890a084c555932b48b74892586ebedc76d552047
26colemanteague-ctrl <26.coleman.teague@carolinaday.org>
2026-01-09 21:35:26 +0000

    I don't know

---
+++ day-3 new code.py
@@ -0,0 +1,23 @@
+num1= float(input("enter you first diget"))
+num2= float(input("enter your second diget"))
+choice = input("what operation would you like?")
+sum = num1+num2
+print(sum)
+print()
+print("below is subtract")
+sum = num1-num2
+print(sum)
+print()
+print("below is multiply")
+sum= num1*num2
+print(sum)
+print()
+print("below is devide")
+sum= num1/num2
+print(sum)
+print()
+print("below is exponets")
+sum= num1**num2
+print(sum)
+print()
+print("below is long division")
