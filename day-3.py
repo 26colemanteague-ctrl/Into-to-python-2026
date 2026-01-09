b76a6c6dd909ee5987897e36c27b7bd4e4467eb9
26colemanteague-ctrl <26.coleman.teague@carolinaday.org>
2026-01-09 15:58:20 +0000

    I don't know any more

---
+++ day-3.py
@@ -0,0 +1,12 @@
+#create 4 varables
+intVar = -1
+floatVar = 3.14
+strVar1 = "hello"
+strVar2 = "world"
+
+print(intVar)
+print(floatVar)
+print(strVar1)
+print(strVar2)
+
+print(str(intVar) +" "" + str(floatVar) + "" + "" + strVar1 + "" + strVar2)
